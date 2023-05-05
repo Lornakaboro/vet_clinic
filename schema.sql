36052f43cab8f0771ee3697513c377676f28a76a
Lornakaboro <lornakaboro@gmail.com>
2023-05-05 23:31:30 +0300

    Create specialists and visit tables

--- schema.sql
+++ schema.sql
@@ -19,3 +19,18 @@ CREATE TABLE vets (
   age INTEGER,
   date_of_graduation DATE
 );
+
+/* Create specialties table */
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+/* Create visits table */
+CREATE TABLE visits (
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE,
+  PRIMARY KEY (animal_id, vet_id, visit_date)
+);
